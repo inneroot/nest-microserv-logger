6b93de237b77be82f650b939b7c53587dbabe758
inneroot <hakooplay@gmail.com>
2020-11-22 18:56:02 +0300

    MessagePattern

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ctx, MessagePattern, NatsContext, Payload } from '@nestjs/microservices'
 
 @Controller()
 export class AppController {
+  private logger = new Logger('MicroService')
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('*')
+  async getHello(@Payload() data: number[], @Ctx() context: NatsContext) {
+    this.logger.log(`reseaved: ${JSON.stringify(data)}`)
+    this.logger.log(context)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Monitoring service')
+
+const microserviceOptions: MicroserviceOptions = {
+  transport: Transport.NATS,
+  options: {
+    url: 'nats://localhost:4222',
+  },
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
+  app.listen(() => logger.log('Microservice is listening...'));
 }
+
 bootstrap();
